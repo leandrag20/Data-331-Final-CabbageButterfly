9a791a7a499d8747633d5752a70e8fdb5f975f65
leandrag20 <leandragottschalk20@augustana.edu>
2022-12-06 20:26:35 -0600

    cleaned data in order to help summarize 1A, 2A

--- Data-Final-RScript.R
+++ Data-Final-RScript.R
@@ -13,12 +13,40 @@ cleanedLWA <- read_excel("Cleaned Data LWA .xlsx", .name_repair = "universal")
 cabbageButterfly <- read_excel("CompletePierisData_2022-03-09 (1).xlsx", .name_repair = "universal")
 
 cabbageButterfly <- cabbageButterfly %>%
-  mutate(SexUpdated = recode(SexUpdated, M = "male", F = "female"))%>%
-  lubridate::parse_date_time(dwc.eventDate, c('Ymd'))
-
+  mutate(SexUpdated = recode(SexUpdated, M = "male", F = "female"))
 
 ladybugMap <- ladybugData %>%
   select(Species, coordinates)%>%
   arrange(Species, desc(Species))
 
+femaleWingLength <- cleanedLWA %>%
+  select(sex, LW.length, RW.length)%>%
+  dplyr::filter(sex == 'female')%>%
+  summarise(averageRight = mean(RW.length), averageLeft = mean(LW.length))
+
+maleWingLength <- cleanedLWA%>%
+  select(sex, LW.length, RW.length)%>%
+  dplyr::filter(sex == 'male')%>%
+  summarise(averageRight = mean(RW.length), averageLeft = mean(LW.length))
+
+femaleWingWidth <- cleanedLWA %>%
+  select(sex, LW.width, RW.width)%>%
+  dplyr::filter(sex == 'female')%>%
+  summarise(averageRight = mean(RW.width), averageLeft = mean(LW.width))
+
+maleWingWidth <- cleanedLWA%>%
+  select(sex, LW.width, RW.width)%>%
+  dplyr::filter(sex == 'male')%>%
+  summarise(averageRight = mean(RW.width), averageLeft = mean(LW.width))
+
+femaleApexArea <- cleanedLWA%>%
+  select(sex, LW.apex.A, RW.apex.A)%>%
+  dplyr::filter(sex == 'female')%>%
+  summarise(averageRight = mean(RW.apex.A), averageLeft = mean(LW.apex.A))
+
+maleApexArea <- cleanedLWA%>%
+  select(sex, LW.apex.A, RW.apex.A)%>%
+  dplyr::filter(sex == 'male')%>%
+  summarise(averageRight = mean(RW.apex.A), averageLeft = mean(LW.apex.A))
+            
 
