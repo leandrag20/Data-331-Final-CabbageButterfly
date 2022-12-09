ba2146858d6ac98c0b9b0f27ee354d2dcabff4e5
leandrag20 <leandragottschalk20@augustana.edu>
2022-12-08 20:29:57 -0600

    refactored to make changes more efficient

--- Data-Final-RScript.R
+++ Data-Final-RScript.R
@@ -8,45 +8,42 @@ rm(list = ls())
 
 setwd("~/git_data/Data-331-Final")
 
-ladybugData <- read_excel("Ladybug Data.xlsx", .name_repair = "universal")
 cleanedLWA <- read_excel("Cleaned Data LWA .xlsx", .name_repair = "universal")
 cabbageButterfly <- read_excel("CompletePierisData_2022-03-09 (1).xlsx", .name_repair = "universal")
 
 cabbageButterfly <- cabbageButterfly %>%
   mutate(SexUpdated = recode(SexUpdated, M = "male", F = "female"))
 
-ladybugMap <- ladybugData %>%
-  select(Species, coordinates)%>%
-  arrange(Species, desc(Species))
+test <- cleanedLWA %>%
+  group_by(sex) %>%
+  summarise(averageRightArea = mean(RW.apex.A),
+            averageLeftArea = mean(LW.apex.A),
+            averageRightWidth = mean(RW.width), 
+            averageLeftWidth = mean(LW.width),
+            averageRightLength = mean(RW.length), 
+            averageLeftLength = mean(LW.length))
 
-femaleWingLength <- cleanedLWA %>%
-  select(sex, LW.length, RW.length)%>%
-  dplyr::filter(sex == 'female')%>%
-  summarise(averageRight = mean(RW.length), averageLeft = mean(LW.length))
+cabbageButterfly$LAnteriorSpotM3 <- as.numeric(cabbageButterfly$LAnteriorSpotM3)
+cabbageButterfly$RAnteriorSpotM3 <- as.numeric(cabbageButterfly$RAnteriorSpotM3)
 
-maleWingLength <- cleanedLWA%>%
-  select(sex, LW.length, RW.length)%>%
-  dplyr::filter(sex == 'male')%>%
-  summarise(averageRight = mean(RW.length), averageLeft = mean(LW.length))
+anteriorSpotDataMale <- cabbageButterfly%>%
+  select(SexUpdated, LAnteriorSpotM3, RAnteriorSpotM3)%>%
+  dplyr::filter(SexUpdated == 'male')%>%
+  na.omit(anteriorSpotDataMale)%>%
+  summarise(rightSpotAverage = mean(LAnteriorSpotM3), leftSpotAverage = mean(RAnteriorSpotM3))
 
-femaleWingWidth <- cleanedLWA %>%
-  select(sex, LW.width, RW.width)%>%
-  dplyr::filter(sex == 'female')%>%
-  summarise(averageRight = mean(RW.width), averageLeft = mean(LW.width))
+anteriorSpotDataFemale <- cabbageButterfly%>%
+  select(SexUpdated, LAnteriorSpotM3, RAnteriorSpotM3)%>%
+  dplyr::filter(SexUpdated == 'female')%>%
+  na.omit(anteriorSpotDataFemale)%>%
+  summarise(rightSpotAverage = mean(LAnteriorSpotM3), leftSpotAverage = mean(RAnteriorSpotM3))
 
-maleWingWidth <- cleanedLWA%>%
-  select(sex, LW.width, RW.width)%>%
-  dplyr::filter(sex == 'male')%>%
-  summarise(averageRight = mean(RW.width), averageLeft = mean(LW.width))
 
-femaleApexArea <- cleanedLWA%>%
-  select(sex, LW.apex.A, RW.apex.A)%>%
-  dplyr::filter(sex == 'female')%>%
-  summarise(averageRight = mean(RW.apex.A), averageLeft = mean(LW.apex.A))
 
-maleApexArea <- cleanedLWA%>%
-  select(sex, LW.apex.A, RW.apex.A)%>%
-  dplyr::filter(sex == 'male')%>%
-  summarise(averageRight = mean(RW.apex.A), averageLeft = mean(LW.apex.A))
+  
+  
+
+
+  
             
 
