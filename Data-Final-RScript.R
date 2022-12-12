3053047d39afaa6bbe847bd1679cf88b9ef5f622
leandrag20 <leandragottschalk20@augustana.edu>
2022-12-12 14:36:54 -0600

    adde territory plots

--- Data-Final-RScript.R
+++ Data-Final-RScript.R
@@ -25,6 +25,13 @@ cabbageButterflySexData <- cleanedLWA %>%
 
 cabbageButterfly$LAnteriorSpotM3 <- as.numeric(cabbageButterfly$LAnteriorSpotM3)
 cabbageButterfly$RAnteriorSpotM3 <- as.numeric(cabbageButterfly$RAnteriorSpotM3)
+cabbageButterfly$RBlackPatchApex <- as.numeric(cabbageButterfly$RBlackPatchApex)
+cabbageButterfly$LBlackPatchApex <- as.numeric(cabbageButterfly$LBlackPatchApex)
+cabbageButterfly$RWingLength <- as.numeric(cabbageButterfly$RWingLength)
+cabbageButterfly$RWingWidth <- as.numeric(cabbageButterfly$RWingWidth)
+cabbageButterfly$LWingLength <- as.numeric(cabbageButterfly$LWingLength)
+cabbageButterfly$LWingWidth <- as.numeric(cabbageButterfly$LWingWidth)
+cabbageButterfly$dwc.month <- as.numeric(cabbageButterfly$dwc.month)
 
 anteriorSpotDataMale <- cabbageButterfly%>%
   select(SexUpdated, LAnteriorSpotM3, RAnteriorSpotM3)%>%
@@ -38,6 +45,8 @@ anteriorSpotDataFemale <- cabbageButterfly%>%
   na.omit(anteriorSpotDataFemale)%>%
   summarise(rightSpotAverageFemale = mean(LAnteriorSpotM3), leftSpotAverageFemale = mean(RAnteriorSpotM3))
 
+cabbageButterfly$dwc.year <- as.numeric(cabbageButterfly$dwc.year)
+na.omit(cabbageButterfly$dwc.year)
 cabbageButterfly$decade <- substring(cabbageButterfly$dwc.year, first = 3, last = 3)
 cabbageButterfly$decade <- paste0(cabbageButterfly$decade, '0')
 
@@ -49,15 +58,10 @@ na.omit(cabbageButterfly$LWingLength)
 na.omit(cabbageButterfly$LWingWidth)
 na.omit(cabbageButterfly$RAnteriorSpotM3)
 na.omit(cabbageButterfly$LAnteriorSpotM3)
-
-cabbageButterfly$RBlackPatchApex <- as.numeric(cabbageButterfly$RBlackPatchApex)
-cabbageButterfly$LBlackPatchApex <- as.numeric(cabbageButterfly$LBlackPatchApex)
-cabbageButterfly$RWingLength <- as.numeric(cabbageButterfly$RWingLength)
-cabbageButterfly$RWingWidth <- as.numeric(cabbageButterfly$RWingWidth)
-cabbageButterfly$LWingLength <- as.numeric(cabbageButterfly$LWingLength)
-cabbageButterfly$LWingWidth <- as.numeric(cabbageButterfly$LWingWidth)
-cabbageButterfly$RAnteriorSpotM3 <- as.numeric(cabbageButterfly$RAnteriorSpotM3)
-cabbageButterfly$LAnteriorSpotM3 <- as.numeric(cabbageButterfly$LAnteriorSpotM3)
+na.omit(cabbageButterfly$SexUpdated)
+na.omit(cabbageButterfly$decade)
+na.omit(cabbageButterfly$dwc.country)
+na.omit(cabbageButterfly$dwc.month)
   
 decadeData <- cabbageButterfly%>%
   dplyr::group_by(decade)%>%
@@ -86,7 +90,62 @@ cabbageButterflySexData2 <- cabbageButterflySexData2[,-c(3)]
 barplot(cabbageButterflySexData2$female, col = "pink", main = 'Female Data', xlab = 'Body Part', ylab = "Millimeters")
 barplot(cabbageButterflySexData2$male, col = "blue", main = 'Male Data', xlab = 'Body Part', ylab = "Millimeters")
 
+territoryData <- cabbageButterfly%>%
+  dplyr::group_by(dwc.country)%>%
+  dplyr::summarise(averageRightArea = mean(RBlackPatchApex), 
+                   averageLeftArea = mean(LBlackPatchApex),
+                   averageRightWidth = mean(RWingWidth), 
+                   averageLeftWidth = mean(LWingWidth),
+                   averageRightLength = mean(RWingLength), 
+                   averageLeftLength = mean(LWingLength),
+                   rightSpotAverage = mean(RAnteriorSpotM3),
+                   leftSpotAverageMale = mean(LAnteriorSpotM3))
 
+territoryData2 <- data.frame(t(territoryData[-1]))
+territoryData2 <- territoryData2[,-c(2,3, 5, 6, 7, 8)]
+colnames(territoryData2)[1] = "Canada"
+colnames(territoryData2)[2] = "USA"
 
+barplot(territoryData2$Canada, col = "blue", main = 'Canada Data', xlab = 'Body Part', ylab = "Millimeters")
+barplot(territoryData2$USA, col = "blue", main = 'USA Data', xlab = 'Body Part', ylab = "Millimeters")
+
+monthData <- cabbageButterfly%>%
+  dplyr::group_by(dwc.month)%>%
+  dplyr::summarise(averageRightArea = mean(RBlackPatchApex), 
+                   averageLeftArea = mean(LBlackPatchApex),
+                   averageRightWidth = mean(RWingWidth), 
+                   averageLeftWidth = mean(LWingWidth),
+                   averageRightLength = mean(RWingLength), 
+                   averageLeftLength = mean(LWingLength),
+                   rightSpotAverage = mean(RAnteriorSpotM3),
+                   leftSpotAverageMale = mean(LAnteriorSpotM3))
+
+yearlyData <- cabbageButterfly%>%
+  dplyr::group_by(YearUpdated)%>%
+  dplyr::summarise(averageRightArea = mean(RBlackPatchApex), 
+                   averageLeftArea = mean(LBlackPatchApex),
+                   averageRightWidth = mean(RWingWidth), 
+                   averageLeftWidth = mean(LWingWidth),
+                   averageRightLength = mean(RWingLength), 
+                   averageLeftLength = mean(LWingLength),
+                   rightSpotAverage = mean(RAnteriorSpotM3),
+                   leftSpotAverageMale = mean(LAnteriorSpotM3))
+
+decadeData3 <- cabbageButterfly%>%
+  dplyr::group_by(decade)%>%
+  filter(decade == "00" || decade == "90" || decade == "10")%>%
+  dplyr::summarise(averageRightWidth = mean(RWingWidth), 
+                   averageLeftWidth = mean(LWingWidth),
+                   averageRightLength = mean(RWingLength), 
+                   averageLeftLength = mean(LWingLength))
+
+decadeData4 <- data.frame(t(decadeData3[-1]))
+colnames(decadeData4)[1] = "00"
+colnames(decadeData4)[2] = "10"
+colnames(decadeData4)[3] = "90"
+
+barplot(decadeData4$`00`, col = "green", main = '00 Decade Data', xlab = 'Body Part', ylab = "Millimeters")
+barplot(decadeData4$`10`, col = "green", main = '10 Decade Data', xlab = 'Body Part', ylab = "Millimeters")
+barplot(decadeData4$`90`, col = "green", main = '90 Decade Data', xlab = 'Body Part', ylab = "Millimeters")
             
 
